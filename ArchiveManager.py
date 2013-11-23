5b818cc0482e6e614b5e77d7332fae62355c654b
Luigi Pirelli <luipir@gmail.com>
2013-11-23 20:28:45 +0100

    correct bug in get feature by gid_catasto

--- ArchiveManager.py
+++ ArchiveManager.py
@@ -545,7 +545,7 @@ class ArchiveManager(QObject):
         self.checkConnection()
     
         # create query
-        sqlquery = "SELECT *,ST_AsText(the_geom) FROM missions_safety WHERE gid_catasto LIKE '_%d_' " % gid_catasto
+        sqlquery = "SELECT *,ST_AsText(the_geom) FROM missions_safety WHERE gid_catasto LIKE '%_" + gid_catasto + "_%' "
         sqlquery += "ORDER BY id;"
         
         QgsLogger.debug(self.tr("Recupera le safety legate al poligono catastale con la query: %s" % sqlquery), 1 )
